9a8bb12b6c780ba2506b09c9c8b5aa926c6e3b6c
yigit kaya <kayayigit2929@gmail.com>
2026-09-12 16:32:58 +0300

    Create lesson.py

---
+++ python_kod/lesson.py
@@ -0,0 +1,72 @@
+# pi=3
+
+# radius_of_circle=float(input("radius:"))
+
+# area_of_circle=pi*radius_of_circle*radius_of_circle
+
+# circumfrence_of_circle=2*pi*radius_of_circle
+
+# print("area:",area_of_circle)
+
+# print("circumference:",circumfrence_of_circle)
+
+# number1=int(input("enter the first number:"))
+# number2=int(input("enter the second number:"))
+
+# sum=number1+number2
+
+# print("is equal to:",sum)
+# import random
+
+# print(random.random()) #rastgele
+
+# state=random.getstate()
+
+# print(random.random()) #kaydetti
+
+# print(random.random()) #rastgele
+
+# random.setstate(state)
+
+# print(random.random()) #2 nin aynısı
+#import random
+
+#mylist=["red","green","blue"]
+
+# text="my home world"
+# search="home"
+# if search in text:
+#     print("it has")
+# else:
+#     print("it has not")
+
+
+# score=int(input("enter your score: "))
+# if score>=90:
+#     print("excellent")
+# elif score>=75:
+#     print("good")
+# elif score>=50:
+#     print("pass")
+# else:
+#     print("fail")
+
+# age=20
+# name="yigit"
+# color="orange"
+# text=f"my name is {name} and my age is {age} and my favorite color is {color}"#f-string
+# print(text)
+
+# price=1.6368
+# text=f"price is {price:.2f} turkish lira" #float gösterimi
+# print(text)
+
+# midterm_score=input("enter midterm score: ")
+# final_score=input("enter final score: ")
+# average_score=((float(midterm_score)+float(final_score))/2)
+# result=f"your midterm score is {midterm_score}\nyour final score is {final_score}\nyour average score is {average_score}"
+# print(result)
+
+text="hello python"
+result=text.center(22,"-")
+print(result)
